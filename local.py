724c484d42cfec77a824d803eeb38560a82ee744
Biozid Bostami <biozid0208@gmail.com>
2021-12-29 22:22:16 -0800

    [save-point] added sample count calculation

--- local.py
+++ local.py
@@ -25,31 +25,67 @@ def csv_parser(file_url):
                 line_count += 1
             else:
                 data_url = row[0]
-                index = int(row[1])
+                lambda_value = int(row[1])
                 covar_url= row[2]
                 line_count += 1
-    return  data_url, index, covar_url
+    return  data_url, lambda_value, covar_url
 
 def local_0(args):
     input_list = args["input"]
     datapath = args["state"]["baseDirectory"] + "/" +  input_list["data"]
-    data_url, index, covar_url = csv_parser(datapath)
+    data_url, lambda_value, covar_url = csv_parser(datapath)
     data_urls = args["state"]["baseDirectory"] + "/" +  data_url
     covar_urls = args["state"]["baseDirectory"] + "/" +  covar_url
     output_dict = {"computation_phase": "local_0"}
-    cache_dict = {"data_urls": data_urls, "location": index, "covar_urls": covar_urls}
+    cache_dict = {"data_urls": data_urls, "lambda_value": lambda_value, "covar_urls": covar_urls}
     computation_output = {"output": output_dict, "cache": cache_dict}
     return json.dumps(computation_output)
 
 def local_1(args):
     covar_url =  args["cache"]["covar_urls"]
-    mat = scipy.io.loadmat(covar_url)
-    X = mat['mod']
-    augmented_X = add_site_covariates(args, X)
-    raise Exception(augmented_X)
-
+    data_url = args["cache"]["data_urls"]
+    lambda_value = args["cache"]["lambda_value"]
+    mat_X = scipy.io.loadmat(covar_url)
+    mat_Y = scipy.io.loadmat(data_url)
     
+    
+    X = mat_X['mod']
+    Y = mat_Y['data']
+    sample_count = len(Y)
+    
+    augmented_X = add_site_covariates(args, X)
+    biased_X = augmented_X.values
+    XtransposeX_local = np.matmul(np.matrix.transpose(biased_X), biased_X)
+    Xtransposey_local = np.matmul(np.matrix.transpose(biased_X), Y)
+    output_dict = {
+        "local_sample_count": sample_count,
+        "XtransposeX_local": XtransposeX_local.tolist(),
+        "Xtransposey_local": Xtransposey_local.tolist(),
+        "lambda_value": lambda_value,
+        "computation_phase": "local_1",
+    }
+    cache_dict = {
+        "covariates": augmented_X.to_json(orient='split'),
+    }
+    computation_output = {"output": output_dict, "cache": cache_dict}
 
+    return json.dumps(computation_output)
+        
+def local_2(args):
+    #ComBat Method starts
+    input_list = args["input"]
+    cache_list = args["cache"]
+    beta_matrix = np.array(input_list["avg_beta_vector"])
+    cache_dict = {"beta_matrix": beta_matrix }
+    output_dict = {
+
+       "computation_phase": "local_2" 
+    }
+
+    computation_output = {"output": output_dict, "cache": cache_dict}
+
+    return json.dumps(computation_output)
+    raise Exception(beta_matrix.shape)
  
 if __name__ == '__main__':
 
@@ -61,6 +97,9 @@ if __name__ == '__main__':
         sys.stdout.write(computation_output)
     elif "remote_0" in phase_key:
         computation_output = local_1(parsed_args)
+        sys.stdout.write(computation_output)
+    elif "remote_1" in phase_key:
+        computation_output = local_2(parsed_args)
         sys.stdout.write(computation_output)
     else:
         raise ValueError("Error occurred at Local")
--- remote.py
+++ remote.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import numpy as np
+import scipy.io as sp
 
 def list_recursive(d, key):
     for k, v in d.items():
@@ -22,6 +23,7 @@ def remote_0(args):
         
     ]
 
+
     output_dict = {
         "site_covar_list": site_covar_list,
         "computation_phase": "remote_0"
@@ -33,8 +35,38 @@ def remote_0(args):
     
     return json.dumps(computation_output)
 
+def remote_1(args):
+    input_list = args["input"]
+    beta_vector_0 = [ np.array(input_list[site]["XtransposeX_local"]) for site in input_list]
+    beta_vector_1 = sum(beta_vector_0)
+    all_lambdas = [input_list[site]["lambda_value"] for site in input_list]
+    if np.unique(all_lambdas).shape[0] != 1:
+        raise Exception("Unequal lambdas at local sites")
+    
+    beta_vector_1 = beta_vector_1 + np.unique(all_lambdas) * np.eye(beta_vector_1.shape[0])   
+    avg_beta_vector = np.matrix.transpose(
+    sum([
+        np.matmul(np.linalg.inv(beta_vector_1),
+                    input_list[site]["Xtransposey_local"])
+        for site in input_list
+    ]))
 
 
+    total_sample_count = sum(input_list[site]["local_sample_count"] for site in input_list)
+
+    
+    output_dict = {
+        "avg_beta_vector": avg_beta_vector.tolist(),
+        "total_count": total_sample_count, 
+        "computation_phase": "remote_1"
+    }
+
+    cache_dict = {
+        "avg_beta_vector": avg_beta_vector.tolist()
+    }
+
+    computation_output = {"output": output_dict, "cache": cache_dict}
+    return json.dumps(computation_output)
 
 if __name__ == '__main__':
     parsed_args = json.loads(sys.stdin.read())
@@ -42,5 +74,8 @@ if __name__ == '__main__':
     if "local_0" in phase_key:
         computation_output = remote_0(parsed_args)
         sys.stdout.write(computation_output)
+    elif "local_1" in phase_key:
+        computation_output = remote_1(parsed_args)
+        sys.stdout.write(computation_output)  
     else:
         raise ValueError("Error occurred at Remote")
